8233ad236e3ecf128da720413fd0dc4a432fd763
Bomge69 <n-shabatura@mail.ru>
2025-09-07 19:32:02 +0300

    Задание 11.1 Научиться работать со множеством TreeSet Реализовал методы класска EmailList: Проверил валидность вводимого email (в этом случае добавляем mail), привел все к нижнему регистру и вернул список имейлов. В классе Main реализоал использование методов класса EmailList через консоль.

--- src/main/java/practice/EmailList.java
+++ src/main/java/practice/EmailList.java
@@ -1,19 +1,31 @@
 package practice;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.TreeSet;
 
 public class EmailList {
+
+    TreeSet<String> emailList = new TreeSet<>();
+
+    public boolean regexEmail(String email) {
+        email = email.toLowerCase();
+        String regex = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$";
+        return email.matches(regex);
+    }
 
     public void add(String email) {
         // TODO: валидный формат email добавляется, email это строка, она быть может любой
         // принять решение добавлять аргумент email или нет должен этот метод
+        email = email.toLowerCase();
+        if (regexEmail(email) && !emailList.contains(email)) {
+            emailList.add(email);
+        }
     }
 
     public List<String> getSortedEmails() {
         // TODO: возвращается сортированный список электронных адресов в алфавитном порядке
-        return new ArrayList<>();
+        ArrayList<String> emList = new ArrayList<>(emailList);
+        return emList;
     }
-
 }
